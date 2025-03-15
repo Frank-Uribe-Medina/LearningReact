a1f373d5eb3becf05e779ca41f0479fb911c494e
CaligoFrank <112025541+CaligoFrank@users.noreply.github.com>
2025-03-14 18:36:52 -0700

    Making the page

--- scripts.js
+++ scripts.js
@@ -1,21 +1,5 @@
-function random_item_from_list(arr){
-    min = 0 
-    max = arr.length - 1
-
-    rand_int = Math.floor(Math.random() * (max - min) + min)
-    return arr[rand_int]
-
-};
-
-
-
-// Changing the Color of the H2 Element
-
-const h1_tag = document.getElementById("rainbow");
-const change_color_buttom = document.getElementById("change-color-button");
-const arr_of_colors = ["red", "green", "yellow"]
-
-change_color_buttom.addEventListener("click", function () {
-  // Code to execute when the button is clicked
-  h1_tag.style.color = random_item_from_list(arr_of_colors)
+const add_to_cart_button = document.getElementById('add-to-cart');
+add_to_cart_button.addEventListener('click', function() {
+  alert('Item added to cart');
+  console.log('Item added to cart');
 });
